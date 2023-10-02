5bc27c57fcb35b40464ecb1076324627b80b5d1b
JoleneLim22019027 <k22019027@kcl.ac.uk>
2023-10-02 15:55:57 +0100

    first commit

--- primes.py
+++ primes.py
@@ -2,5 +2,28 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    try:
+        list = []
+        num = 2
+        if number_of_primes >= 1:
+            list = [2]
+        while (len(list) < number_of_primes):
+            pFlag = True
+            i = 0
+            while (pFlag and (i < len(list)) and (list[i] <= num)):
+                if (num % list[i] == 0):
+                    pFlag = False
+                else:
+                    i += 1
+            if pFlag:
+                list.append(num)
+            
+            num += 1
+
+        return list
+    
+    except ValueError:
+        print("Input cannot be 0 or a negative number!")
+        return []
+
+
